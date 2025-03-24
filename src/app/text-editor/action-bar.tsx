014d23e12d13918079c5b45797ad01ec5b85a6e3
unknown <michaelxaviercanonizado@gmail.com>
2025-03-24 23:10:00 +0800

    chore(action-bar): initialize component

---
+++ src/app/text-editor/action-bar.tsx
@@ -0,0 +1,7 @@
+import { cn } from '@/lib/utils';
+
+const ActionBar = ({ className }: { className?: string }) => {
+	return <div className={cn(className, '')}>ActionBar</div>;
+};
+
+export default ActionBar;
