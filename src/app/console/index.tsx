55b705dafa7c5632ad0bc009c04057f22a509c8a
unknown <michaelxaviercanonizado@gmail.com>
2025-03-24 23:13:50 +0800

    feat(console): add temporary styles

--- src/app/console/index.tsx
+++ src/app/console/index.tsx
@@ -1,7 +1,11 @@
 import { cn } from '@/lib/utils';
 
 const Console = ({ className }: { className?: string }) => {
-	return <div className={cn(className, '')}>Console</div>;
+	return (
+		<div className={cn(className, 'text-xl grid place-items-center')}>
+			Console
+		</div>
+	);
 };
 
 export default Console;
