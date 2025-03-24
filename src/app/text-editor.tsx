52ad9e71cb067746c8958e471a5b2b370b6cb250
unknown <michaelxaviercanonizado@gmail.com>
2025-03-24 21:36:01 +0800

    chore: initialize component

---
+++ src/app/text-editor.tsx
@@ -0,0 +1,8 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+const TextEditor = ({ className }: { className: string }) => {
+	return <div className={cn(className, '')}>TextEditor</div>;
+};
+
+export default TextEditor;
