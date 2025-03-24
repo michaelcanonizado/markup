e49422d17e1362465bef6f9ec76f99287d3024f3
unknown <michaelxaviercanonizado@gmail.com>
2025-03-24 21:57:15 +0800

    fix(globals.css): revert the theme back to the original

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Console from './console';
 
 export default function Home() {
 	return (
-		<main className="bg-purple-500 min-h-screen grid grid-cols-2">
+		<main className="min-h-screen grid grid-cols-2">
 			<TextEditor />
 			<Console />
 		</main>
