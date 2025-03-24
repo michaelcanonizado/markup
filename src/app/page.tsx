1afdb73b5ae8a29e65b5547621374759a1a9a0ca
unknown <michaelxaviercanonizado@gmail.com>
2025-03-24 22:15:06 +0800

    feat: add Resizable component to split the TextEditor and Console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup,
+} from '@/components/ui/resizable';
+
 import TextEditor from './text-editor';
 import Console from './console';
 
 export default function Home() {
 	return (
-		<main className="min-h-screen grid grid-cols-2">
-			<TextEditor />
-			<Console />
+		<main className=" h-screen px-8 pb-8 flex flex-col">
+			<div className="py-8">
+				<h1 className="text-4xl font-bold">Markup</h1>
+			</div>
+			<ResizablePanelGroup
+				direction="horizontal"
+				className="growrounded-lg border"
+			>
+				<ResizablePanel defaultSize={50}>
+					<TextEditor className="text-xl w-full h-full  grid place-items-center" />
+				</ResizablePanel>
+				<ResizableHandle withHandle />
+				<ResizableHandle />
+				<ResizablePanel defaultSize={50}>
+					<Console className="text-xl w-full h-full  grid place-items-center" />
+				</ResizablePanel>
+			</ResizablePanelGroup>
 		</main>
 	);
 }
