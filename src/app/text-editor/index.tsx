6faa8eba22f0295c15006eab86c82973b3b4b882
unknown <michaelxaviercanonizado@gmail.com>
2025-03-25 11:45:12 +0800

    feat(text-editor): add monaco-editor to TextEditor component

--- src/app/text-editor/index.tsx
+++ src/app/text-editor/index.tsx
@@ -1,11 +1,35 @@
+'use client';
+
 import { cn } from '@/lib/utils';
 import ActionBar from './action-bar';
+import Editor from '@monaco-editor/react';
+import { editor } from 'monaco-editor';
+import { useRef } from 'react';
 
 const TextEditor = ({ className }: { className?: string }) => {
+	const textEditorRef = useRef<null | editor.IStandaloneCodeEditor>(null);
+
+	const getTextEditorValue = () => {
+		if (textEditorRef.current == null) {
+			return;
+		}
+
+		console.log(textEditorRef.current.getValue());
+	};
+
 	return (
 		<div className={cn(className, 'text-xl flex flex-col')}>
 			<ActionBar />
-			<div className="grow grid place-items-center">Text Editor</div>
+			<div className="grow grid place-items-center">
+				<Editor
+					height="100%"
+					defaultLanguage=""
+					defaultValue="// some comment"
+					onMount={(editor) => {
+						textEditorRef.current = editor;
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
