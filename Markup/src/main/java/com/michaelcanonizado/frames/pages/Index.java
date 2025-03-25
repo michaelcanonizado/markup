866e6ce14e105dc27b1264a1ca9112c6161c652b
unknown <michaelxaviercanonizado@gmail.com>
2025-03-25 22:06:32 +0800

    feat: add custom window title bar

--- Markup/src/main/java/com/michaelcanonizado/frames/pages/Index.java
+++ Markup/src/main/java/com/michaelcanonizado/frames/pages/Index.java
@@ -15,6 +15,7 @@ public class Index extends javax.swing.JFrame {
      */
     public Index() {
         initComponents();
+        simpleTitleBar1.init(this);
     }
 
     /**
@@ -26,11 +27,19 @@ public class Index extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        simpleTitleBar2 = new javaswingdev.SimpleTitleBar();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
         jPanel1 = new javax.swing.JPanel();
+        simpleTitleBar1 = new javaswingdev.SimpleTitleBar();
+
+        jScrollPane1.setViewportView(jTextPane1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(9, 9, 11));
         setName("index"); // NOI18N
+        setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(1024, 650));
         setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(9, 9, 11));
@@ -39,11 +48,13 @@ public class Index extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1024, Short.MAX_VALUE)
+            .addComponent(simpleTitleBar1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1024, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 768, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(simpleTitleBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 622, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -54,7 +65,7 @@ public class Index extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -97,5 +108,9 @@ public class Index extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextPane jTextPane1;
+    private javaswingdev.SimpleTitleBar simpleTitleBar1;
+    private javaswingdev.SimpleTitleBar simpleTitleBar2;
     // End of variables declaration//GEN-END:variables
 }
