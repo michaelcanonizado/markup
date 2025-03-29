73386af2f0c48bb82080680ebaa538f31fb259d9
unknown <michaelxaviercanonizado@gmail.com>
2025-03-30 02:09:43 +0800

    chore(StatementData): initialize class

---
+++ Markup/src/main/java/com/michaelcanonizado/markup/utils/StatementData.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.michaelcanonizado.markup.utils;
+
+import java.util.List;
+
+/**
+ *
+ * @author lilac
+ */
+public class StatementData {
+    private List<String> escapeSequences;
+    private String text = null;
+
+    public StatementData(List<String> escapeSequences, String text) {
+        this.escapeSequences = escapeSequences;
+        this.text = text;
+    }
+    
+    @Override
+    public String toString() {
+        return "EscapeSequences: " + escapeSequences + ", Text: \"" + text + "\"";
+    }
+}
